ca07737697b034254aa4bdda290bc308a8bfce3a
Quentin Ormancey <quentin@ormancey.fr>
2020-04-18 23:37:51 +0200

    RoleSync config implementation

--- rolesync/rolesync.py
+++ rolesync/rolesync.py
@@ -13,15 +13,22 @@ class RoleSync(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, 143056204359270400)
         asyncio.ensure_future(self.init_config())
 
     async def init_config(self):
-        try:
-            self.main_guild = await self.config.MAIN_GUILD()
-        except:
-            self.main_guild = 0
-            await self.config.MAIN_GUILD.set(self.main_guild)
+        self.config = Config.get_conf(self, 143056204359270400)
+
+        default_global = {
+            "Main_Guild": 0
+        }
+        default_guild = {
+            "Wolf_Role": 0,
+            "Solo_Role": 0
+        }
+        self.config.register_global(**default_global)
+        self.config.register_guild(**default_guild)
+
+        self.main_guild = await self.config.Main_Guild()
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -42,4 +49,17 @@ class RoleSync(commands.Cog):
     @_set.command()
     async def mainguild(self, ctx):
         self.main_guild = int(ctx.guild.id)
-        await self.config.MAIN_GUILD.set(self.main_guild)
+        await self.config.Main_Guild.set(self.main_guild)
+        ctx.send(f"Serveur principal définit sur `{ctx.guild.name}`")
+
+    @_set.command()
+    async def wolfrole(self, ctx):
+        role = ctx.message.role_mentions[0]
+        await self.config.guild(ctx.guild).Wolf_Role.set(role.id)
+        ctx.send(f"Role loup définit sur `{role.mention}`")
+
+    @_set.command()
+    async def solorole(self, ctx):
+        role = ctx.message.role_mentions[0]
+        await self.config.guild(ctx.guild).Solo_Role.set(role.id)
+        ctx.send(f"Role solitaire définit sur `{role.mention}`")
