f04a29231841d5bb690e603783777c010a7c4fff
Quentin Ormancey <quentin@ormancey.fr>
2020-05-23 20:08:37 +0200

    Suppression du loup solitaire + admin

--- rolesync/rolesync.py
+++ rolesync/rolesync.py
@@ -13,7 +13,6 @@ class RoleSync(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         asyncio.ensure_future(self.init_config())
-        self.rolecheck_task = self.bot.loop.create_task(self.rolecheckauto())
 
     async def init_config(self):
         """Init cogs config"""
@@ -24,16 +23,12 @@ class RoleSync(commands.Cog):
         }
         default_guild = {
             "Wolf_Role": 0,
-            "Member_Role": 0,
-            "Solo_Role": 0
+            "Member_Role": 0
         }
         self.config.register_global(**default_global)
         self.config.register_guild(**default_guild)
 
         self.main_guild = self.bot.get_guild(await self.config.Main_Guild())
-
-    def cog_unload(self):
-        self.rolecheck_task.cancel()
 
     async def get_colour(self, channel):
         """Get Bot's main colour"""
@@ -46,70 +41,18 @@ class RoleSync(commands.Cog):
             for guild in self.bot.guilds:
                 if guild != self.main_guild:
                     if not guild.get_member(member.id) is None:
-                        solo_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Solo_Role())
-                        if solo_role in guild.get_member(member.id).roles:
-                            await guild.get_member(member.id).remove_roles(solo_role, reason="L'utilisateur a rejoint le serveur principal")
-                            wolf_role = discord.utils.get(member.guild.roles, id=await self.config.guild(member.guild).Wolf_Role())
-                            await member.add_roles(wolf_role, reason="L'utilisateur est wolf")
+                        wolf_role = discord.utils.get(member.guild.roles, id=await self.config.guild(member.guild).Wolf_Role())
+                        await member.add_roles(wolf_role, reason="L'utilisateur est wolf")
         else:
             if not self.main_guild.get_member(member.id) is None:
-                if self.main_guild.get_role(await self.config.guild(self.main_guild).Admin_Role()) in self.main_guild.get_member(member.id).roles:
-                    admin_role = discord.utils.get(member.guild.roles, id=await self.config.guild(member.guild).Admin_Role())
-                    await member.add_roles(admin_role, reason="L'utilisateur est admin")
                 if self.main_guild.get_role(await self.config.guild(self.main_guild).Wolf_Role()) in self.main_guild.get_member(member.id).roles:
                     if not member.guild.get_role(await self.config.guild(member.guild).Member_Role()) in member.roles:
                         member_role = discord.utils.get(member.guild.roles, id=await self.config.guild(member.guild).Member_Role())
                         await member.add_roles(member_role, reason="L'utilisateur est membre")
 
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        """Member leave event"""
-        if member.guild == self.main_guild:
-            for guild in self.bot.guilds:
-                if guild != self.main_guild:
-                    wolf_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Wolf_Role())
-                    if not guild.get_member(member.id) is None:
-                        if wolf_role in guild.get_member(member.id).roles:
-                            solo_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Solo_Role())
-                            await guild.get_member(member.id).add_roles(solo_role, reason="L'utilisateur a quitté le serveur principal")
-
     @commands.group(name="rolesync")
     async def rolesync(self, ctx):
         """Commande principale de RoleSync"""
-    
-    @rolesync.command()
-    async def forcerolecheck(self, ctx):
-        """Force la recherche et attribution du role solitaire"""
-        embed = discord.Embed(colour=await self.get_colour(ctx.message.channel), title="Attribution des rôles en cours", description="Merci de patienter")
-        work_message = await ctx.send(embed=embed)
-        count = 0
-        for guild in self.bot.guilds:
-            if guild != self.main_guild:
-                wolf_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Wolf_Role())
-                for member in guild.members:
-                    if wolf_role in member.roles:
-                        if not member in self.main_guild.members:
-                            solo_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Solo_Role())
-                            await member.add_roles(solo_role, reason="L'utilisateur n'est pas sur le serveur principal")
-                            count += 1
-        embed = discord.Embed(colour=0x00ff40, title="Attibution terminée", description=f"{count} roles attribués")
-        await work_message.edit(embed=embed)
-    
-    async def rolecheckauto(self):
-        """Force la recherche et attribution du role solitaire"""
-        while self == self.bot.get_cog("RoleSync"):
-            count = 0
-            for guild in self.bot.guilds:
-                if guild != self.main_guild:
-                    wolf_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Wolf_Role())
-                    for member in guild.members:
-                        if wolf_role in member.roles:
-                            if not member in self.main_guild.members:
-                                solo_role = discord.utils.get(guild.roles, id=await self.config.guild(guild).Solo_Role())
-                                await member.add_roles(solo_role, reason="L'utilisateur n'est pas sur le serveur principal")
-                                count += 1
-            await asyncio.sleep(3600)
 
     @rolesync.group(name="set")
     async def _set(self, ctx):
@@ -121,16 +64,6 @@ class RoleSync(commands.Cog):
         await self.config.Main_Guild.set(int(ctx.guild.id))
         await self.init_config()
         await ctx.send(f"Serveur principal définit sur `{ctx.guild.name}`")
-
-    @_set.command()
-    async def adminrole(self, ctx):
-        """Definit le role admin de ce serveur"""
-        if len(ctx.message.role_mentions) == 0:
-            await ctx.send("Veuillez mentionner un role")
-            return
-        role = ctx.message.role_mentions[0]
-        await self.config.guild(ctx.guild).Admin_Role.set(role.id)
-        await ctx.send(f"Role admin définit sur {role.mention}")
 
     @_set.command()
     async def wolfrole(self, ctx):
@@ -152,16 +85,6 @@ class RoleSync(commands.Cog):
         await self.config.guild(ctx.guild).Member_Role.set(role.id)
         await ctx.send(f"Role membre définit sur {role.mention}")
 
-    @_set.command()
-    async def solorole(self, ctx):
-        """Definit le role solitaire de ce serveur"""
-        if len(ctx.message.role_mentions) == 0:
-            await ctx.send("Veuillez mentionner un role")
-            return
-        role = ctx.message.role_mentions[0]
-        await self.config.guild(ctx.guild).Solo_Role.set(role.id)
-        await ctx.send(f"Role solitaire définit sur {role.mention}")
-    
     @commands.guild_only()
     @commands.command(pass_context=True)
     async def set_admin(self, ctx):
