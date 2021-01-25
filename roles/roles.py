144b66f5a7495d37fef4eb20197750b1a79e6c50
Quentin Ormancey <23664359+louga31@users.noreply.github.com>
2021-01-25 22:40:48 +0100

    Part 1

--- roles/roles.py
+++ roles/roles.py
@@ -1,17 +1,61 @@
-from typing import cast
+from dataclasses import dataclass
+from typing import List, Any, TypeVar, Callable, Type, cast
 import discord
 import asyncio
+import locale
+from discord import embeds
+from discord import reaction
 from redbot.core import commands, Config
 from redbot.core.bot import RedBase
-import locale
-locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
 
-listener = getattr(commands.Cog, "listener", None)  # red 3.0 backwards compatibility support
+T = TypeVar("T")
+locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
+listener = getattr(commands.Cog, 'listener', None)  # red 3.0 backwards compatibility support
 
 if listener is None:  # thanks Sinbad
     def listener(name=None):
         return lambda x: x
 
+def from_list(f: Callable[[Any], T], x: Any) -> List[T]:
+    assert isinstance(x, list)
+    return [f(y) for y in x]
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+def from_role(guild: discord.Guild, x: Any) -> discord.Role:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return guild.get_role(x)
+
+def from_emoji(guild: discord.Guild, x: Any) -> discord.Emoji:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return guild.fetch_emoji(x)
+
+def to_id(x: Any) -> int:
+    assert isinstance(x, discord.Role) or isinstance(x, discord.Emoji)
+    return x.id
+
+@dataclass
+class RoleMessage:
+    roles: List[discord.Role]
+    emojis: List[discord.Emoji]
+    status: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'RoleMessage':
+        assert isinstance(obj, dict)
+        roles = from_list(from_role, obj.get("roles"))
+        emojis = from_list(from_emoji, obj.get("emojis"))
+        status = from_int(obj.get("status"))
+        return RoleMessage(roles, emojis, status)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["roles"] = from_list(to_id, self.roles)
+        result["emojis"] = from_list(to_id, self.emojis)
+        result["status"] = from_int(self.status)
+        return result
 
 class Roles(commands.Cog):
     """Roles commands"""
@@ -22,17 +66,23 @@ class Roles(commands.Cog):
         asyncio.ensure_future(self.set_roles())
 
     async def set_roles(self):
-        self.roles = await self.config.ROLES()
-    
+        self.role_messages = await self.config.role_messages()
+        if self.role_messages is None:
+            self.role_messages = {}
+            await self.config.role_messages.set(self.role_messages)
+
     async def get_colour(self, channel):
         return await RedBase.get_embed_colour(self.bot, channel)
     
     async def give_role(self, role: discord.Role, member: discord.Member):
-        await member.add_roles(role, reason="Self give")
+        await member.add_roles(role, reason='Self give')
         return
 
     async def remove_role(self, role, member):
-        await member.remove_roles(role, reason="Self remove")
+        await member.remove_roles(role, reason='Self remove')
+        return
+    
+    async def process_step(self):
         return
 
     @listener()
@@ -76,14 +126,22 @@ class Roles(commands.Cog):
         #     role = guild.get_role(int(opt_dict[str(payload.emoji)].split('&')[1].split('>')[0]))
         #     await self.remove_role(role, member)
         pass
-
+        
     @commands.guild_only()       
     @commands.command(pass_context=True)
-    async def rolemessage(self, ctx, messageid: int, *roles: str):
+    async def rolemessage(self, ctx, message: discord.Message, *roles: discord.Role):
+        print(roles)
         await ctx.message.delete()
         if len(roles) < 1:
-            await ctx.send('You need at least 1 role!')
+            embed=discord.Embed(title='You need at least 1 role!', color=0xff0000)
+            await ctx.send(embed=embed)
             return
         if len(roles) > 10:
-            await ctx.send('You cannot make a selfrole message for more than 10 roles!')
+            embed=discord.Embed(title='You cannot make a selfrole message for more than 10 roles!', color=0xff0000)
+            await ctx.send(embed=embed)
             return
+        self.role_messages[message.id] = RoleMessage(list(roles), [], 1).to_dict()
+        await self.config.role_messages.set(self.role_messages)
+        print(self.role_messages)
+        embed = discord.Embed(colour=await self.get_colour(message.channel), title=f"Merci d'ajouter à ce message la réaction que vous voulez pour le rôle {self.role_messages[message.id].roles[self.role_messages[message.id].status]}")
+        await message.channel.send(embed=embed)
